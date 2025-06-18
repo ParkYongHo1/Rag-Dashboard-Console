9df1fd8ee7546debf241957d970ea267170db118
ParkYongHo1 <qkaxhf8823@naver.com>
2025-06-18 22:21:27 +0900

    Chore | 배포 에러 수정

--- vite.config.ts
+++ vite.config.ts
@@ -10,16 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    // 테스트 환경 설정
-    environment: "jsdom",
-    // 테스트 파일 패턴
-    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    // 글로벌 설정 (describe, it, expect 등을 import 없이 사용)
-    globals: true,
-    // 테스트 셋업 파일
-    setupFiles: "./src/test/setup.ts",
-  },
+
   server: {
     port: 5173,
     open: true,
