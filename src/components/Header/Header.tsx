0eb9625806fa5226c86fe16e27b4506aaafbae44
ParkYongHo1 <qkaxhf8823@naver.com>
2025-07-06 12:51:19 +0900

    Feat | 로그아웃 버튼 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import { LinkButton } from "@/shared/ui/LinkButton";
 import { useCompanyStore } from "@/stores/companyStore";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation();
   const currentPath = location.pathname;
   const isAuthenticated = useCompanyStore((state) => state.isAuthenticated);
   const company = useCompanyStore((state) => state.company);
+  const logout = useCompanyStore((state) => state.logout);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <div className="w-full shadow-md flex flex-col h-[10vh] bg-white">
       <div className="flex items-center justify-between px-6 py-5 h-[10vh] border-b border-gray-200">
@@ -43,11 +49,17 @@ const Header = () => {
               />
             </>
           )}
-
-          <div className="font-bold">
-            {(isAuthenticated && company?.company) ||
-              "로그인 후 이용가능합니다"}
-          </div>
+          {isAuthenticated && (
+            <>
+              <button
+                onClick={handleLogout}
+                className="px-4 py-2 text-gray-400 hover:text-black hover:cursor-pointer"
+              >
+                LOGOUT
+              </button>
+              <div className="font-bold">{company?.company}</div>
+            </>
+          )}
         </div>
       </div>
     </div>
